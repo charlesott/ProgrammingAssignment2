d1c4f9a51258f3bed3a4245c02ef8c0f1d4a178e
charlesott <charlesott@yahoo.com>
2014-06-18 10:49:18 -0400

    modified contents to submit my work to coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions work together to cache a matrix and its inverse
 
-## Write a short comment describing this function
-
+## Accepts a matrix and returns a list of functions for use in
+## cacheSolve()
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        ## initialize the cached variable in this contructor
+        csh_inv_mtrx <- NULL
+        
+        ## set the matrix
+        csh_mtrx <- x
+        
+        ## set the values of the matrix and return. Don't compute solve here
+        set <- function (v, r, c){
+                ## if we're calling set, the constructor is not going to
+                ## clear the cached variable or set a matrix for us
+                csh_inv_mtrx <<- NULL
+                csh_mtrx <<- x
+        }
+        
+        ## return the matrix created
+        get <- function() csh_mtrx
+                
+        ## function for setting the cashed inverted value
+        set_inv_mtrx <- function(inv_mtrx) csh_inv_mtrx <<- inv_mtrx
+        
+        ## function for getting the cashed inverted value
+        get_inv_mtrx <- function() csh_inv_mtrx
+        
+        ## return
+        list(set = set,
+             get = get,
+             set_inv_mtrx = set_inv_mtrx,
+             get_inv_mtrx = get_inv_mtrx)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Accepts an instance of the makeCacheMatrix() object and returns either the
+## cached version of the inverse, or computes/stores a new inverse if one 
+## doesn't already exist in cache
+cacheSolve <- function(x, ...){
+        ## attempt to get the value of the cached inverted matrix     
+        invmtrx <- x$get_inv_mtrx()
+        
+        if (!is.null(invmtrx)) {
+                message("getting cached data")
+                return(invmtrx)
+        }
+        
+        data <- x$get()
+        invmtrx <- solve(data)
+        x$set_inv_mtrx(invmtrx)
+        invmtrx
 }
+
